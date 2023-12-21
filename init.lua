5c3119d302ed01a98c8fc570876b9a225358ca88
sugan0tech <sugankpms@gmail.com>
2023-12-21 22:37:37 +0530

    added neotree and plugin rearchitecture

--- init.lua
+++ init.lua
@@ -17,23 +17,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts) -- loads lazy with plugins and options
+require("lazy").setup("plugins") -- loads lazy with plugins and options
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- for neo tree
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+vim.keymap.set('n', '<C-Shift-e>', ':Neotree filesystem reveal left<CR>', {})
 
 local config = require("nvim-treesitter.configs")
 config.setup({
