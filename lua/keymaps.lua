4492ec12c3c6468f61caf0856f002c463376bec1
sugan0tech <sugankpms@gmail.com>
2023-12-27 23:13:45 +0530

    added clipboard shortcut mapping

---
+++ lua/keymaps.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true }) -- maps to new L-y map
+vim.api.nvim_set_keymap("n", "<leader>p", '"+p', { noremap = true, silent = true})
