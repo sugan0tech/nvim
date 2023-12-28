96d26882a3edc9390458a46c32979d9fb8354a0b
sugan0tech <sugankpms@gmail.com>
2023-12-28 13:33:53 +0530

    added auto install for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,8 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "java", "rust", "go", "elixir", "haskell", "ruby", "python", "typescript"},
+      ensure_installed = { "lua", "javascript", "java", "rust", "go", "elixir", "haskell", "ruby", "python", "typescript" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true }
     })
