15b1b26bce999efc9c0d64b2432df23575f336c7
sugan0tech <sugankpms@gmail.com>
2023-12-28 23:08:03 +0530

    nvim-cmp scroll update

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -51,6 +51,8 @@ M.config = function()
 			documentation = cmp.config.window.bordered(),
 		},
 		mapping = cmp.mapping.preset.insert({
+      ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+      ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
 			["<C-Space>"] = cmp.mapping.complete(),
