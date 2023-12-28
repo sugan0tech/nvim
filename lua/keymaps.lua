363517d7a78bf0661af4871da61fab0f2840047e
sugan0tech <sugankpms@gmail.com>
2023-12-28 21:19:19 +0530

    refactored keymaps and added keymaps for macros

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,4 @@
-vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true }) -- maps to new L-y map
-vim.api.nvim_set_keymap("n", "<leader>p", '"+p', { noremap = true, silent = true})
+vim.keymap.set('n', 'Q', "@qj") -- applying macro and enter new line
+vim.keymap.set('x', 'Q', ':norm @q<CR>') -- applying macro to multiple lines
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>p', '"+p')
