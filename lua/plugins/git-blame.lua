45ed174cb96dea9a06ea1220bc140f779446e6c2
sugan0tech <sugankpms@gmail.com>
2023-12-30 13:10:39 +0530

    added advanced git blame plugin

---
+++ lua/plugins/git-blame.lua
@@ -0,0 +1,7 @@
+return {
+	"f-person/git-blame.nvim",
+	config = function()
+		--[[ vim.keymap.set("n", "<leader>gbc", ":GitBlameOpenCommitUrl<CR>")
+		vim.keymap.set("n", "<leader>gbf", ":GitBlameOpenFileUrl<CR>") ]]
+	end,
+}
