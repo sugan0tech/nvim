dc8b5e49a5a060196b0ac3a2a7dfcc0c7a48b99d
sugan0tech <sugankpms@gmail.com>
2023-12-31 17:56:19 +0530

    added colorizer

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,11 @@
+return {
+  'norcalli/nvim-colorizer.lua',
+  config = function ()
+    require("nvim-colorizer").setup({
+      "css";
+      ".tmux.conf";
+      "js";
+      "java";
+    })
+  end
+}
