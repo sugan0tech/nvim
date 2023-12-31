a7bd63fc8713b5620100945d50474aaf0e5bc573
sugan0tech <sugankpms@gmail.com>
2023-12-31 20:42:05 +0530

    added follow_current_file flag in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,20 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
-    vim.keymap.set("n", "<leader>fs", ":Neotree filesystem reveal left<CR>", {})
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>fs", ":Neotree filesystem reveal left<CR>", {})
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+		})
+	end,
 }
