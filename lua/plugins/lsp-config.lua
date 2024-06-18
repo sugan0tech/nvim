0e03bec62ba0546bd520ba1c5a32169d2e30b0c5
sugan0tech <sugankpms@gmail.com>
2024-06-18 19:22:09 +0530

    add: snippets for html

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "html", "jsonls", "jdtls", "kotlin_language_server", "gopls" , "pyright", "svelte", "rust_analyzer"}
+        ensure_installed = { "lua_ls", "tsserver", "html", "jsonls", "jdtls", "kotlin_language_server", "gopls" , "pyright", "svelte", "rust_analyzer", "eslint", "cssls"}
       })
     end
   },
@@ -29,6 +29,7 @@ return {
       lspconfig.pyright.setup({})
       lspconfig.svelte.setup({})
       lspconfig.rust_analyzer.setup({})
+      lspconfig.eslint.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
       vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
